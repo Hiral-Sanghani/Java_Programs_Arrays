46ec2c8da90193605b378ab8bfe5019774562462
Hiral-Sanghani <sanghanihiral1211@gmail.com>
2024-06-18 08:51:28 +0530

    Find Sum and Average of the array elements

--- src/main/java/org/example/Sum_Of_Array_Elements.java
+++ src/main/java/org/example/Sum_Of_Array_Elements.java
@@ -3,6 +3,18 @@ package org.example;
 //To Find Sum of array elements
 public class Sum_Of_Array_Elements {
     public static void main(String[] args) {
+        int sum = 0;
+        int average;
+        int[] arr = new int[] {3,2,1};
+
+        for(int a : arr) {
+            sum = sum + a; // alternatively : sum+= a;
+        }
+        System.out.println("Sum : " + sum);
+
+        average = sum / arr.length;
+
+        System.out.println("Average : " + average);
 
     }
 }
